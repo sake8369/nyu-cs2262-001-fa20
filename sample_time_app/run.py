043d5e1ac5c78e734213d8e6d6a9b4afdee91337
Yi-Wen <sake8369@gmail.com>
2020-10-01 17:17:24 -0400

    lab2

--- sample_time_app/run.py
+++ sample_time_app/run.py
@@ -1,10 +1,17 @@
 from flask import Flask
+from datetime import datetime
+
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello world!'
+    return "Hello World!"
+
+@app.route('/time')
+def show_time():
+    time2 = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    return time2
 
 
 app.run(host='0.0.0.0',
